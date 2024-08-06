1311dcf971f5fffeeef4ac034cfb51e155b64548
Manish Mishra <55812397+Mishra1997@users.noreply.github.com>
2024-08-06 14:28:25 -0700

    wip

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -2,12 +2,30 @@ import React,{useState} from "react";
 import Header from './Header'
 import Footer from './Footer'
 import Login from "./Login";
+import Card from 'react-bootstrap/Card';
 const LandingPage =()=>{
 
 
   return( <>
 
-This is landing page
+
+
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+ 
+
+export default TextExample;
     </>
   )
 
