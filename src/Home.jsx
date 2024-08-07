f0d6865497522902d02699e244da0cfa8e255709
Manish Mishra <55812397+Mishra1997@users.noreply.github.com>
2024-08-07 13:16:07 -0700

    wip

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,13 +7,55 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import "./Home.css"
 const Home =()=>{
   
 
   return( <>
 
-<div>
+<div className="mainSec">
+<div className="leftSec"> <Card.Body>
+        <Card.Title>WelCome</Card.Title>
+        {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
+        <Card.Text>
+         
+        </Card.Text>
+  manish
+      </Card.Body></div>
+<div className="centerSec"> <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
 
+        </Card.Text>
+  
+      </Card.Body> </div>
+<div className="leftSec"> <Card.Body>
+        <Card.Title>Graphical representation  Comming Soon..</Card.Title>
+        {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
+        <Card.Text>
+      
+        </Card.Text>
+  
+      </Card.Body></div>
 </div>
 
     </>
